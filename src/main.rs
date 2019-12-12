9295927d0904f74e739e266cc1001724eb01808c
Bernd-L <18219148+Bernd-L@users.noreply.github.com>
2019-12-12 23:18:28 +0100

    Lots of progress; compiles

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,7 @@ fn read(input: &mut String) {
  * Calculates the value of the digit used to check
  * the validity of the number, given the number
  */
-fn calculateValidationDigit(digits: &Vec<u8>) -> u8 {
-  return 42;
+fn calculateValidationDigit(digits: &[u8]) -> u8 {
   /*
     Example:
       1234 01 01 80
@@ -58,4 +57,45 @@ fn calculateValidationDigit(digits: &Vec<u8>) -> u8 {
 
       X = 7
   */
+
+  /**
+   * The array after multiplication
+   */
+  let mut transformed_digits = [0; 10];
+
+  // Transform the incrementing digits
+  transformed_digits[0] = digits[0] * 3;
+  transformed_digits[1] = digits[1] * 7;
+  transformed_digits[2] = digits[2] * 9;
+
+  // Transform the birthday digits
+  // TODO cache these calculations
+  transformed_digits[4] = digits[4] * 5;
+  transformed_digits[5] = digits[5] * 8;
+  transformed_digits[6] = digits[6] * 4;
+  transformed_digits[7] = digits[7] * 2;
+  transformed_digits[8] = digits[8] * 1;
+  transformed_digits[9] = digits[9] * 6;
+
+  /**
+   * The sum mod 11
+   */
+  let x = (transformed_digits[0]
+    + transformed_digits[1]
+    + transformed_digits[2]
+    + transformed_digits[4]
+    + transformed_digits[5]
+    + transformed_digits[6]
+    + transformed_digits[7]
+    + transformed_digits[8]
+    + transformed_digits[9])
+    % 11;
+
+  // Avoid x being 10
+  if x == 10 {
+    // TODO increment the incrementing number (the first 3 digits) by one
+    return calculateValidationDigit(&digits);
+  }
+
+  return x;
 }
