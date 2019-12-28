69e33e64e950bc9e7ca40dbae5d389fb5bf927e0
Bernd-L <18219148+Bernd-L@users.noreply.github.com>
2019-12-28 01:07:41 +0100

    Window title

--- src/gui.rs
+++ src/gui.rs
@@ -1,48 +1,23 @@
 extern crate gtk;
 use gtk::prelude::*;
-use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
 
 pub fn main() {
-  // // When something went wrong
-  // if gtk::init().is_err() {
-  //   println!("Failed to initialize GTK.");
-  //   return;
-  // }
-
-  // // Assuming the init of GTK was successful
-  // let popup = MessageDialog::new(
-  //   None::<&Window>,
-  //   DialogFlags::empty(),
-  //   MessageType::Info,
-  //   ButtonsType::Ok,
-  //   "Hello World",
-  // );
-
-  // popup.run();
-
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-
+  // Initialize GTK
   if gtk::init().is_err() {
+    // When something went wrong
     println!("Failed to initialize GTK.");
     return;
   }
+
+  // Create a window based on the Glade XML file
   let glade_src = include_str!("layout/Design.glade");
   let builder = gtk::Builder::new_from_string(glade_src);
 
   let window: gtk::Window = builder.get_object("window1").unwrap();
-  let btFix: gtk::Button = builder.get_object("btFix").unwrap();
+  let bt_fix: gtk::Button = builder.get_object("btFix").unwrap();
   // let dialog: gtk::MessageDialog = builder.get_object("messagedialog1").unwrap();
 
-  btFix.connect_clicked(move |_| println!("button clicked"));
+  bt_fix.connect_clicked(move |_| println!("button clicked"));
 
   // button.connect_clicked(move |_| {
   //   dialog.run();
